3b4446b71881e75eccbd27f0c7a0e82035c6a962
iethan-h <ethanharwell34@gmail.com>
2021-11-18 17:46:28 -0600

    finished locations store

--- react-app/src/store/location.js
+++ react-app/src/store/location.js
@@ -1,4 +1,9 @@
+/* eslint-disable no-unused-vars */
 const ADD_LOCATION = '/locations/ADD_LOCATION'
+const GET_ONE_LOCATION = '/locations/GET_ONE_LOCATION'
+const GET_ALL_LOCATIONS = '/locations/GET_ALL_LOCATIONS'
+const REMOVE_LOCATION = '/locations/REMOVE_LOCATION'
+const EDIT_LOCATION = '/locations/EDIT_LOCATION'
 
 
 /*-------------ACTIONS-------------*/
@@ -9,8 +14,128 @@ const addOneLocation = location =>({
     location
 })
 
+const getALocation = location =>({
+    type:GET_ONE_LOCATION,
+    
+    location
+})
+
+const getAllLocations = location =>({
+    type:GET_ALL_LOCATIONS,
+    
+    location
+})
+
+const removeLocation = location =>({
+    type:REMOVE_LOCATION,
+    
+    location
+})
+
+const editALocation = location =>({
+    type:EDIT_LOCATION,
+    
+    location
+})
+
 /*-------------THUNKS-------------*/
 
-export const newHost = () => async (dispatch) => {
-    
+export const newLocation = (payload, userId, imageUrl) => async (dispatch) => {
+    const response = await fetch(`/api/locations`, {
+        method: 'POST',
+        headers: {
+            'Content-Type' : 'application/json',
+          },
+        body: JSON.stringify({...payload, userId, imageUrl})
+    })
+    if (response.ok) {
+        const location = await response.json()
+        dispatch(addOneLocation(location))
+    }
+}
+
+export const AllLocations = (locations) => async(dispatch)=> {
+    const response = await fetch(`/api/locations`);
+	const data = await response.json();
+	dispatch(getAllLocations(data));
+}
+
+export const getSingleLocation = (location) => async (dispatch) => {
+    const res = await fetch(`/api/locations/${location}`)
+    const oneLocation= await res.json();
+    dispatch(getALocation(oneLocation))
+}
+
+export const editLocation = (location) => async (dispatch) => {
+    const response = await fetch(`/api/locations/${location}`, {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(location),
+      });
+  
+      if (response.ok) {
+        const spot = await response.json();
+        dispatch(editALocation(location));
+        return location;
+      }
+}
+
+export const deleteLocation = (locationId) => async (dispatch) =>{
+    const response = await fetch(`/api/locations/${locationId}`, {
+        method: 'DELETE',
+        headers: { 'Content-Type': 'application/json',}
+    })
+    dispatch(removeLocation(locationId))
+    return response;
+}
+
+/*-------------REDUCER-------------*/
+
+const LocationReducer = (state = {}, action) =>{
+    let newState;
+    switch(action.type) {
+        
+        
+        case GET_ALL_LOCATIONS:
+            newState = {...state}
+            action.locations.forEach(location => {
+                newState[location?.id] = location
+            });
+            return newState;
+            
+            
+        case ADD_LOCATION:
+            console.log('action clg', action.location);
+            return {
+                ...state,
+                [action.location?.id]: action.location
+            }
+            
+            
+        case REMOVE_LOCATION: {
+            const newState = { ...state };
+            delete newState[action.location];
+            return newState;
+            }
+            
+            
+        case EDIT_LOCATION: {
+            return{
+            ...state,
+            [action.location.id]: action.location
+        }}
+        
+        
+        case GET_ONE_LOCATION:
+            return {
+                ...state,
+             [action.spot.id]: action.spot
+         }
+         
+         
+        default:
+            return state;
+    }
 }
