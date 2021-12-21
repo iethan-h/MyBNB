c8f1edc6f483a1344d0166258a4eb6363a8e3468
iethan-h <ethanharwell34@gmail.com>
2021-12-21 12:04:32 -0600

    splash page styling

--- react-app/src/components/splashPage/index.js
+++ react-app/src/components/splashPage/index.js
@@ -1,4 +1,7 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable jsx-a11y/alt-text */
 import './splash.css'
+import ReactTypingEffect from 'react-typing-effect';
 
 const Splash = () => {
     return(
@@ -6,7 +9,12 @@ const Splash = () => {
             <div className="image">
                 <img className='img1' src="https://loveincorporated.blob.core.windows.net/contentimages/gallery/a0b24016-8626-4c51-9016-f763a428e0c0-this-man-built-a-treehouse-1.jpg"/> 
             </div>
-            <h1 className="name">Mybnb</h1>
+            <ReactTypingEffect className="name"
+            text={["Mybnb","A Project By Ethan Harwell"]}
+            eraseDelay={1500}
+            typingDelay={1500}
+            speed={200}
+            />
             <div>
                 <img className='img2' src='https://track-pm.s3.amazonaws.com/luxurycoastalvacations/image/1d88c424-4f72-41c2-afda-2992b51b42c0'/>
             </div>
